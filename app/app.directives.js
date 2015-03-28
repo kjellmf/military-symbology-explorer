2d80db36e89189fad1536734f3a3cc7df105f9b8
Kjell Magne Fauske <kjellmf@gmail.com>
2015-03-28 15:29:57 +0100

    Auto select text in search input field

--- app/app.directives.js
+++ app/app.directives.js
@@ -154,5 +154,18 @@ angular.module('symbolApp')
                 });
             }
         }
-    }]);
+    }])
+
+    .directive('autofocusSelect', ['$timeout', function ($timeout) {
+        return {
+            restrict: 'A',
+            link: function ($scope, $element) {
+                $timeout(function () {
+                    $element[0].focus();
+                    $element[0].select();
+                });
+            }
+        }
+    }])
+;
 
