0102e668ba24d94a9014139fe570982d4d745a70
Kjell Magne Fauske <kjellmf@gmail.com>
2017-05-21 12:07:32 +0200

    Add the about view

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import MainView from './views/main-view.vue';
 import FramesView from './views/frames-view.vue';
+// import ExploreView from './views/explore-view.vue';
+import AboutView from './views/about-view.vue';
 
 Vue.use(VueRouter);
 
@@ -9,16 +11,17 @@ const Dummy = { template: '<div>{{ $route.params}}</div>' };
 export default new VueRouter({
     linkActiveClass: 'active',
     mode: 'history',
-    scrollBehavior: function(to, from, savedPosition) {
+    scrollBehavior: function (to, from, savedPosition) {
         if (to.hash) {
-            return {selector: to.hash}
+            return { selector: to.hash }
         } else {
             return { x: 0, y: 0 }
         }
     },
     routes: [
         { path: '/', component: MainView },
-        { path: '/explore/:sic?', name: "explore", component:Dummy},
-        { path: '/frames', name: "frames", component:FramesView}
+        { path: '/about', component: AboutView },
+        { path: '/explore/:sic?', name: "explore", component: Dummy },
+        { path: '/frames', name: "frames", component: FramesView }
     ]
 });
