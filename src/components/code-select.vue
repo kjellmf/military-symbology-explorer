c3327835827ccf841c0322a542cad24fe7f5e5e3
Kjell Magne Fauske <kjellmf@gmail.com>
2017-06-25 20:38:51 +0200

    Use object instead of id as select value

--- src/components/code-select.vue
+++ src/components/code-select.vue
@@ -1,39 +1,36 @@
 <template>
     <select class="form-control" v-model="myValue">
-        <option v-for="val in values" :value="val.id" :key="val.id">
+        <option v-for="val in values" :value="val" :key="val.id">
             {{val.digits}} {{val.label}}
         </option>
     </select>
 </template>
-  
+
 
 <script lang="ts">
 import { Component, Prop, Watch } from 'vue-property-decorator';
 import Vue from "vue";
-import {findWithAttr} from '../jmsml'
 
-@Component({
-
-})
+@Component({})
 export default class CodeSelect extends Vue {
     myValue = null;
     @Prop()
     values;
     @Prop()
     value;
+
     created() {
-        this.myValue = this.value.id;
+        this.myValue = this.value;
     }
 
-    @Watch('value') 
-    onVChange(nn){
-        this.myValue = nn.id;
+    @Watch('value')
+    onVChange(nn) {
+        this.myValue = nn;
     }
 
     @Watch("myValue")
     onValueChange(newValue) {
-        let obj = findWithAttr(this.values, 'id', newValue)
-        this.$emit('input', obj);
+        this.$emit('input', newValue);
     }
 }
 </script>
