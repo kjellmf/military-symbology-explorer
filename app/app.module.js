5f0ee5550afa3fb9e48c6385c0e93213a2360fc3
Kjell Magne Fauske <kjellmf@gmail.com>
2018-03-13 17:38:23 +0100

    Remove old version files

--- app/app.module.js
+++
@@ -1,9 +0,0 @@
-angular.module('symbolApp', ['ngAnimate', 'ui.router', 'ngClipboard', 'mgcrea.ngStrap', 'ngSanitize'])
-    .constant("config", {
-        BLANK_PATH: "assets/img/blank.png",
-        SVG_PATH: "svg/MIL_STD_2525D_Symbols/"
-    })
-
-    .config(['ngClipProvider', function (ngClipProvider) {
-        ngClipProvider.setPath("bower_components/zeroclipboard/dist/ZeroClipboard.swf");
-    }]);
