619484034b0e4320f757db88e8d5757d1d19ebd1
Kjell Magne Fauske <kjellmf@gmail.com>
2018-03-15 18:27:38 +0100

    Create main App.vue

--- src/main.ts
+++ src/main.ts
@@ -3,45 +3,13 @@ import Vue from 'vue'
 import MilSymb from "./components/milsymbol.vue";
 import router from './router';
 import store from './store';
-import {INITIALIZE, SET_SYMBOL_FROM_SIC} from "./store/action-types";
+
+import App from './App.vue';
 
 Vue.component('milsymbol', MilSymb);
 
-const app = new Vue({
-    el: '#app',
-    template: `<div>
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" ng-init="navCollapsed = true"
-                        ng-click="navCollapsed = !navCollapsed">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <router-link class="navbar-brand" to="/">Symbology explorer</router-link>
-            </div>
-            <div id="navbar" class="navbar-collapse collapse" ng-class="!navCollapsed && 'in'"
-                 ng-click="navCollapsed=true">
-                <ul class="nav navbar-nav navbar-left">
-                    <li><router-link to="explore">Explore</router-link></li>
-                    <li><router-link to="symbolset">Symbol set browser</router-link></li>
-                    <li><router-link to="frames">Frames</router-link></li>
-                    <li><router-link to="search">Search</router-link></li>
-                    <li><router-link to="about">About</router-link></li>
-                    <li><a href="https://github.com/kjellmf/military-symbology-explorer">Github project</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <router-view></router-view>
-</div>`,
+new Vue({
     router,
     store,
-
-    created() {
-        this.$store.dispatch(SET_SYMBOL_FROM_SIC, '10031000161211050000');
-    }
-
-});
+    render: (h) => h(App),
+}).$mount('#app');
