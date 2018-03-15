b51c27249632b6951b64665e61d6680d7d72588c
Kjell Magne Fauske <kjellmf@gmail.com>
2018-03-15 19:08:47 +0100

    Clean up main navigation

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <template>
     <v-app id="app">
         <v-navigation-drawer app clipped fixed v-model="drawer">
-            <orbat-sidebar></orbat-sidebar>
         </v-navigation-drawer>
         <v-toolbar app fixed clipped-left dense>
             <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
             <v-toolbar-title>Symbology explorer</v-toolbar-title>
             <v-toolbar-items class="pl-4">
+                <v-btn :to="'/'" flat>Home</v-btn>
                 <v-btn :to="'explore'" flat>Explore</v-btn>
                 <v-btn :to="'symbolset'" flat>Symbol set browser</v-btn>
                 <v-btn :to="'frames'" flat>Frames</v-btn>
-                <v-btn :to="'about'" flat>About</v-btn>
-                <v-btn href="https://github.com/kjellmf/military-symbology-explorer" flat>GitHub</v-btn>
             </v-toolbar-items>
 
             <v-spacer/>
+            <v-toolbar-items>
+                <v-btn :to="'about'" flat>About</v-btn>
+                <v-btn href="https://github.com/kjellmf/military-symbology-explorer" flat>GitHub</v-btn>
+            </v-toolbar-items>
 
         </v-toolbar>
         <v-content>
@@ -24,48 +26,6 @@
         </v-content>
     </v-app>
 </template>
-
-
-<!--<template>-->
-<!--<div>-->
-<!--<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">-->
-<!--<div class="container-fluid">-->
-<!--<div class="navbar-header">-->
-<!--<button type="button" class="navbar-toggle" ng-init="navCollapsed = true"-->
-<!--ng-click="navCollapsed = !navCollapsed">-->
-<!--<span class="sr-only">Toggle navigation</span>-->
-<!--<span class="icon-bar"></span>-->
-<!--<span class="icon-bar"></span>-->
-<!--<span class="icon-bar"></span>-->
-<!--</button>-->
-<!--<router-link class="navbar-brand" to="/">Symbology explorer</router-link>-->
-<!--</div>-->
-<!--<div id="navbar" class="navbar-collapse collapse" ng-class="!navCollapsed && 'in'"-->
-<!--ng-click="navCollapsed=true">-->
-<!--<ul class="nav navbar-nav navbar-left">-->
-<!--<li>-->
-<!--<router-link to="explore">Explore</router-link>-->
-<!--</li>-->
-<!--<li>-->
-<!--<router-link to="symbolset">Symbol set browser</router-link>-->
-<!--</li>-->
-<!--<li>-->
-<!--<router-link to="frames">Frames</router-link>-->
-<!--</li>-->
-<!--<li>-->
-<!--<router-link to="search">Search</router-link>-->
-<!--</li>-->
-<!--<li>-->
-<!--<router-link to="about">About</router-link>-->
-<!--</li>-->
-<!--<li><a href="https://github.com/kjellmf/military-symbology-explorer">Github project</a></li>-->
-<!--</ul>-->
-<!--</div>-->
-<!--</div>-->
-<!--</nav>-->
-<!--<router-view></router-view>-->
-<!--</div>-->
-<!--</template>-->
 
 <script lang="ts">
 import Vue from "vue";
