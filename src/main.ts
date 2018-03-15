b097acf34c4942f63186a64a9aff741388a763da
Kjell Magne Fauske <kjellmf@gmail.com>
2018-03-15 18:54:32 +0100

    Add vuetify and start UI reimplementation

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,11 @@ import router from './router';
 import store from './store';
 
 import App from './App.vue';
-
+import Vuetify from "vuetify";
+import 'vuetify/dist/vuetify.min.css'
 Vue.component('milsymbol', MilSymb);
+
+Vue.use(Vuetify);
 
 new Vue({
     router,
