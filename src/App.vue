194665292d9dac9096cf69f804bf5b9c45278d0f
Kjell Magne Fauske <kjellmf@gmail.com>
2018-03-17 08:43:24 +0100

    Update main view

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,22 @@
         </v-navigation-drawer>
         <v-toolbar app fixed clipped-left dense>
             <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-            <v-toolbar-title>Symbology explorer</v-toolbar-title>
+            <v-toolbar-title>
+                <span class="hidden-sm-and-down">Symbology explorer</span></v-toolbar-title>
             <v-toolbar-items class="pl-4">
                 <v-btn :to="'/'" flat>Home</v-btn>
                 <v-btn :to="'explore'" flat>Explore</v-btn>
-                <v-btn :to="'symbolset'" flat>Symbol set browser</v-btn>
+                <v-btn :to="'symbolset'" flat><span class="hidden-sm-and-down">Symbol set</span> browser</v-btn>
                 <v-btn :to="'frames'" flat>Frames</v-btn>
             </v-toolbar-items>
-
             <v-spacer/>
+            <div class="hidden-sm-and-down">
+
             <v-toolbar-items>
                 <v-btn :to="'about'" flat>About</v-btn>
                 <v-btn href="https://github.com/kjellmf/military-symbology-explorer" flat>GitHub</v-btn>
             </v-toolbar-items>
+            </div>
 
         </v-toolbar>
         <v-content>
--- src/views/main-view.vue
+++ src/views/main-view.vue
@@ -1,97 +1,103 @@
 <template>
-<div class="container-fluid">
-    <div class="row">
-
-        <div class="col-sm-12 col-md-12">
-        <header class="jumbotron hero-spacer">
-                <h1>Joint military symbology explorer</h1>
-
-            <p>An experimental MIL-STD-2525D/APP6X symbology explorer based on the <a
-                href="https://github.com/Esri/joint-military-symbology-xml">Joint Military Symbology Markup Language (JMSML)</a>
-                project.</p>
-
-                <div>
-                    <router-link to="/explore/10131000001211050000"><milsymbol sic="10131000001211050000"></milsymbol></router-link>
-                    <router-link to="/explore/10233000001201000000"><milsymbol sic="10233000001201000000"></milsymbol></router-link>
-                    <router-link to="/explore/10051000001211040000"><milsymbol sic="10051000001211040000"></milsymbol></router-link>
-                    <router-link to="/explore/10031030141211040000"><milsymbol sic="10031030141211040000"></milsymbol></router-link>
-                    <router-link to="/explore/10031500321112030000"><milsymbol sic="10031500321112030000"></milsymbol></router-link>
-                    <router-link to="/explore/10031000161211002019"><milsymbol sic="10031000161211002019"></milsymbol></router-link>
-                    <router-link to="/explore/10033000001500000000"><milsymbol sic="10033000001500000000"></milsymbol></router-link>
-                    <router-link to="/explore/10031000001110980000"><milsymbol sic="10031000001110980000"></milsymbol></router-link>
-                    <router-link to="/explore/10031002181211020000"><milsymbol sic="10031002181211020000"></milsymbol></router-link>
-                </div>
-                <p>&nbsp;</p>
-            </header>
-    </div>
-    </div>
-     <!-- Page Features -->
-        <div class="row text-center">
-
-            <div class="col-md-3 col-sm-6 hero-feature">
-                <div class="thumbnail">
-
-                    <div class="caption ">
-                        <h3>Explore symbol</h3>
-                        <p>Learn symbol identification codes</p>
-                        <p>
-                            <router-link to="explore/" class="btn btn-primary">Explore</router-link>
-                        </p>
+    <v-container grid-list-lg>
+        <v-layout row wrap>
+            <v-flex xs12>
+                <v-jumbotron color="grey lighten-2" >
+                    <div class="px-3 py-3">
+                        <h3 class="display-3">Joint military symbology explorer</h3>
+                        <span class="subheading">An experimental MIL-STD-2525D/APP6X symbology explorer based on the <a
+                            href="https://github.com/Esri/joint-military-symbology-xml">Joint Military Symbology Markup Language (JMSML)</a>
+                project.</span>
+                        <v-divider class="my-3"></v-divider>
+                        <div>
+                            <router-link to="/explore/10131000001211050000">
+                                <milsymbol sic="10131000001211050000"></milsymbol>
+                            </router-link>
+                            <router-link to="/explore/10233000001201000000">
+                                <milsymbol sic="10233000001201000000"></milsymbol>
+                            </router-link>
+                            <router-link to="/explore/10051000001211040000">
+                                <milsymbol sic="10051000001211040000"></milsymbol>
+                            </router-link>
+                            <router-link to="/explore/10031030141211040000">
+                                <milsymbol sic="10031030141211040000"></milsymbol>
+                            </router-link>
+                            <router-link to="/explore/10031500321112030000">
+                                <milsymbol sic="10031500321112030000"></milsymbol>
+                            </router-link>
+                            <router-link to="/explore/10031000161211002019">
+                                <milsymbol sic="10031000161211002019"></milsymbol>
+                            </router-link>
+                            <router-link to="/explore/10033000001500000000">
+                                <milsymbol sic="10033000001500000000"></milsymbol>
+                            </router-link>
+                            <router-link to="/explore/10031000001110980000">
+                                <milsymbol sic="10031000001110980000"></milsymbol>
+                            </router-link>
+                            <router-link to="/explore/10031002181211020000">
+                                <milsymbol sic="10031002181211020000"></milsymbol>
+                            </router-link>
+                        </div>
                     </div>
-                </div>
-            </div>
+                </v-jumbotron>
+            </v-flex>
 
-            <div class="col-md-3 col-sm-6 hero-feature">
-                <div class="thumbnail">
-                    <div class="caption">
-                        <h3>Browse symbol set</h3>
-                        <p>See available symbols</p>
-                        <p>
-                            <router-link to="symbolset" class="btn btn-primary">Browse</router-link>
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-3 col-sm-6 hero-feature">
-                <div class="thumbnail">
-                    <div class="caption">
-                        <h3>Search</h3>
-                        <p>Search for symbols and modifiers</p>
-                        <p>
-                            <router-link to="search" class="btn btn-primary">Search</router-link>
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-3 col-sm-6 hero-feature">
-                <div class="thumbnail">
-                    <div class="caption">
-                        <h3>Frames</h3>
-                        <p>See dimension and standard identity</p>
-                        <p>
-                            <router-link to="frames" class="btn btn-primary">Show frames</router-link>
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-
-        </div>
-        <!-- /.row -->
-
-
-</div>
+            <v-flex>
+                <v-card>
+                    <v-card-title primary-title>
+                        <h3 class="headline ">Explore symbols</h3>
+                    </v-card-title>
+                    <v-card-text>Learn symbol identification codes</v-card-text>
+                    <v-card-actions>
+                        <v-btn flat color="orange" to="explore/">Explore</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+            <v-flex>
+                <v-card>
+                    <v-card-title primary-title>
+                        <h3 class="headline ">Frames</h3>
+                    </v-card-title>
+                    <v-card-text>See dimension and standard identities</v-card-text>
+                    <v-card-actions>
+                        <v-btn flat color="orange" to="frames/">Show frames</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+            <v-flex>
+                <v-card>
+                    <v-card-title primary-title>
+                        <h3 class="headline ">Search</h3>
+                    </v-card-title>
+                    <v-card-text>Search for symbols and modifers</v-card-text>
+                    <v-card-actions>
+                        <v-btn flat color="orange" to="search/">Search</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+            <v-flex>
+                <v-card>
+                    <v-card-title primary-title>
+                        <h3 class="headline ">Browse symbol sets</h3>
+                    </v-card-title>
+                    <v-card-text>See available symbols</v-card-text>
+                    <v-card-actions>
+                        <v-btn flat color="orange" to="symbolset/">Browse</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
 
 </template>
 
 <script lang="ts">
-    import Vue from 'vue';
-    import {Component} from 'vue-property-decorator';
-    @Component
-    export default class MainView extends Vue {
-    }
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class MainView extends Vue {
+}
 </script>
 
 <style scoped>
