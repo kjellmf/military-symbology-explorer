12f5bc5f693e9927009a72095bcd906c1e8c4da6
Kjell Magne Fauske <kjellmf@gmail.com>
2018-04-28 13:11:25 +0200

    Add navigation to drawer

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,58 @@
 <template>
   <v-app id="app">
     <v-navigation-drawer app clipped fixed v-model="drawer">
-      <v-list>
-          <settings/>
+      <v-list dense>
+        <v-subheader>Navigation</v-subheader>
+        <v-list-tile to="/">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile to="explore">
+          <v-list-tile-action>
+            <v-icon>explore</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Explore</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile to="symbolset">
+          <v-list-tile-action>
+            <v-icon>reorder</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Browse symbol set</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile to="frames">
+          <v-list-tile-action>
+            <v-icon>filter_frames</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Frames</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <settings/>
       </v-list>
     </v-navigation-drawer>
     <v-toolbar app fixed clipped-left dense>
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title>
-        <span class="hidden-sm-and-down">Symbology explorer</span></v-toolbar-title>
-      <v-toolbar-items class="pl-4">
+        <span>Symbology explorer</span></v-toolbar-title>
+      <v-toolbar-items class="pl-4 hidden-sm-and-down">
         <v-btn :to="'/'" flat>Home</v-btn>
         <v-btn :to="'explore'" flat>Explore</v-btn>
         <v-btn :to="'symbolset'" flat><span class="hidden-sm-and-down">Symbol set</span> browser</v-btn>
         <v-btn :to="'frames'" flat>Frames</v-btn>
       </v-toolbar-items>
       <v-spacer/>
-      <div class="hidden-sm-and-down">
-
-        <v-toolbar-items>
-          <v-btn :to="'about'" flat>About</v-btn>
-          <v-btn href="https://github.com/kjellmf/military-symbology-explorer" flat>GitHub</v-btn>
-        </v-toolbar-items>
-      </div>
-
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn :to="'about'" flat>About</v-btn>
+        <v-btn href="https://github.com/kjellmf/military-symbology-explorer" flat>GitHub</v-btn>
+      </v-toolbar-items>
     </v-toolbar>
     <v-content>
       <v-container fluid>
@@ -40,7 +69,7 @@ import settings from './components/settings.vue';
 import {SET_SYMBOL_FROM_SIC} from "./store/action-types";
 
 export default Vue.extend({
-  components: {settings},
+  components: { settings },
   data: () => ({
     drawer: true
   }),
